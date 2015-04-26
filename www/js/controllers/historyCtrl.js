cb142223c39686416e361fddb9999ca6d5ee3cf5
Abiodun <abiodun.shuaib.m@gmail.com>
2015-04-26 18:29:55 +0100

    changes

--- www/js/controllers/historyCtrl.js
+++ www/js/controllers/historyCtrl.js
@@ -12,4 +12,10 @@ angular.module('fastFood')
   .controller('historyCtrl', function ($scope,$http) {
 		$scope.history = JSON.parse(window.localStorage['transaction'] || '[]');
 		console.log($scope.history);
+		$scope.clear = function(){
+			if(confirm("Are You sure You want to clear history")){
+				window.localStorage['transaction'] = [];
+				$scope.history = [];
+			}
+		}
   });
